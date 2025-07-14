a6c8e7532a4f47a29c0901a00fbe2594b07e9efd
123 <123>
2025-07-14 20:31:50 +0800

    0714-1

--- dist/js/calculate.js
+++ dist/js/calculate.js
@@ -4,9 +4,20 @@ let num1 = document.querySelector("#num1");
 let num2 = document.querySelector("#num2");
 let operator = document.querySelector("#operator");
 let calcBtn = document.querySelector("#calc-btn");
-let resultvalue = document.querySelector("#result-value");
+let resultDOM = document.querySelector("#result");
+let resultWrap = document.querySelector("#result-wrap");
+console.log(num1, num2, operator, calcBtn, resultDOM, resultWrap);
+resultDOM.textContent = "";
+
+function initResultWrap() {
+    resultWrap.classList.add("hide");
+}
+function showResultWrap() {
+    resultWrap.classList.remove("hide");
+}
 
 const calcRun = () => {
+    initResultWrap();
     // 1. 取得使用者輸入的數字
     let value1 = num1.value;
     let value2 = num2.value;
@@ -40,11 +51,31 @@ const calcRun = () => {
 
         // 4. 顯示結果到 UI 上
         console.log(result);
-        resultvalue.textContent = result;
+        resultDOM.textContent = result;
+        showResultWrap();
     } else {
-        alert("請輸入數字");
+        let errorDOM = "";
+        let errorMsg = "";
+        if (value1 == "") {
+            errorDOM = num1;
+            errorMsg = "請輸入數字1";
+        }
+
+        if (!errorDOM && value2 == "") {
+            errorDOM = num2;
+            errorMsg = "請輸入數字2";
+        }
+
+        Swal.fire({
+            title: "計算錯誤",
+            text: errorMsg,
+            icon: "error",
+            confirmButtonText: "確定",
+        }).then(() => {
+            setTimeout(() => {
+                errorDOM.focus();
+            }, 500);
+        });
     }
 };
-
-// console.log(calcBtn);
 calcBtn.addEventListener("click", calcRun);
