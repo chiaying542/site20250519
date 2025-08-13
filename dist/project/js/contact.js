22a94a1ba88b59c9164a49faf4a2bb70bc2ab42b
0813 <0813>
2025-08-13 21:33:04 +0800

    0813-5

--- dist/project/js/contact.js
+++ dist/project/js/contact.js
@@ -7,7 +7,9 @@
 import { Header } from "./components/Header.js";
 import { Favicon } from "./components/Favicon.js";
 import { Footer } from "./components/Footer.js";
+import { Banner } from "./components/Banner.js";
 
 Header.init("#header-container", "contact");
 Footer.init("#footer-container");
+Banner.init("#banner-container");
 Favicon.init();
--- dist/project/js/news-detail.js
+++ dist/project/js/news-detail.js
@@ -7,7 +7,9 @@
 import { Header } from "./components/Header.js";
 import { Favicon } from "./components/Favicon.js";
 import { Footer } from "./components/Footer.js";
+import { Banner } from "./components/Banner.js";
 
 Header.init("#header-container", "news-detail");
 Footer.init("#footer-container");
+Banner.init("#banner-container");
 Favicon.init();
