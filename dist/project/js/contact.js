f304037aad8861f113c976c97a239e1fd4a41832
0813 <0813>
2025-08-13 18:54:50 +0800

    0813-1

--- dist/project/js/contact.js
+++ dist/project/js/contact.js
@@ -1,5 +1,8 @@
-import { Loader } from "./components/Loader.js";
-import { Header } from "./components/Header.js";
+// import { Loader } from "./components/Loader.js";
+// import { Header } from "./components/Header.js";
 
-await Loader.html("components/header.html", "#header-container");
-Header.activeMenu("contact");
+// await Loader.html("components/header.html", "#header-container");
+// Header.activeMenu("contact");
+
+import { Header } from "./components/Header.js";
+Header.init("#header-container", "contact");
--- dist/project/js/news.js
+++ dist/project/js/news.js
@@ -1,5 +1,8 @@
-import { Loader } from "./components/Loader.js";
-import { Header } from "./components/Header.js";
+// import { Loader } from "./components/Loader.js";
+// import { Header } from "./components/Header.js";
 
-await Loader.html("components/header.html", "#header-container");
-Header.activeMenu("news");
+// await Loader.html("components/header.html", "#header-container");
+// Header.activeMenu("news");
+
+import { Header } from "./components/Header.js";
+Header.init("#header-container", "news");
