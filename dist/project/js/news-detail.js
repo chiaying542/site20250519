1cef2e18ad3fa5f9050355788d91764fc8a88133
0813 <0813>
2025-08-13 21:36:44 +0800

    0813-6

--- dist/project/js/news-detail.js
+++ dist/project/js/news-detail.js
@@ -1,9 +1,3 @@
-// import { Loader } from "./components/Loader.js";
-// import { Header } from "./components/Header.js";
-
-// await Loader.html("components/header.html", "#header-container");
-// Header.activeMenu("news-detail");
-
 import { Header } from "./components/Header.js";
 import { Favicon } from "./components/Favicon.js";
 import { Footer } from "./components/Footer.js";
--- dist/project/js/partner.js
+++ dist/project/js/partner.js
@@ -1,9 +1,3 @@
-// import { Loader } from "./components/Loader.js";
-// import { Header } from "./components/Header.js";
-
-// await Loader.html("components/header.html", "#header-container");
-// Header.activeMenu("partner");
-
 import { Header } from "./components/Header.js";
 import { Favicon } from "./components/Favicon.js";
 import { Footer } from "./components/Footer.js";
